5e87f8f0f375b8707bd39c8ea611199e2bc96cb2
Mustofa KP <mustofakastria11@gmail.com>
2021-03-08 04:01:18 +0700

    php basic

---
+++ string.php
@@ -0,0 +1,32 @@
+<?php
+
+//Single quote
+$string1 = 'Saya sedang melakukan streaming';
+
+echo $string1;
+echo "<br>";
+
+//Double code
+$string2 = "Ini adalah double code";
+echo $string2;
+echo "<br>";
+//end of string
+
+//
+$waktu = 6;
+
+echo $waktu;
+echo "<br>";
+
+//float
+$hargajual = 94.5;
+$hargabeli = 40.3;
+
+echo "saya membeli doge coin dengan harga $hargabeli setelah dijual harganya $hargajual";
+echo "<br>";
+
+//boolean
+$benar = TRUE;
+$salah = FALSE;
+
+echo "benar = $benar, salah = $salah";
---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = "topipeci";
+$umur = 21;
+
+echo $nama;
+echo "<br>";
+echo $umur;
+echo "<br>";
+echo "Halo, perkenalkan nama saya " . $nama . " dan saya berumur " . $umur;
